d9b838cb022fcc953fb30d10f8cec7ba9470fe75
PriyankaBtech <officialpriyanka2000@gmail.com>
2024-02-12 13:22:33 +0530

    basic functions database query

--- BasicMethods/basicFunctions.js
+++ BasicMethods/basicFunctions.js
@@ -42,3 +42,53 @@ async function readMoviesByDirector(directorName) {
 readMoviesByDirector("Rajkumar Hirani")
 
 
+// Create a function that accepts a movies ID and an object with the updated data and updated the movie with provided.
+// Update
+
+async function updateMovie(movieId, updatedData) {   
+    try {
+        const updatedMovie = await Movie.findByIdUpdate(movieId, updatedMovie, { new: true })
+        // Movie.findByIdUpdate({ title: title }, updateMovie, { new: true })
+        console.log("Updated Movie", updatedMovie)
+    } catch (error) {
+        throw error        
+    } 
+    
+}
+
+updateMovie("Your-movie-id", { rating: 8.5 })
+
+
+
+// Create a function that accepts a movie Id and deletes the movie with the provided Id.
+// Delete
+
+async function DeleteMovie(movieId) {
+    try {
+        const movie = await Movie.findByIdDelete(movieId)
+        console.log("Deleted Movie", movie)        
+    } catch (error) {
+        throw error        
+    }
+}
+
+DeleteMovie("Your-movie-id")
+
+
+
+// A function to add data to the "shampoo" model
+
+async function addShampooData() {
+    const newshampoo = new addShampooData({
+        brand: "MamaEarth",
+        price: 299
+    })
+
+    try {
+        const savedShampoo = await newshampoo.save()
+        console.log("shampoo data save successfully", savedShampoo)        
+    } catch (error) {
+        console.log("shampoo data failed to save", error)        
+    }
+}
+
